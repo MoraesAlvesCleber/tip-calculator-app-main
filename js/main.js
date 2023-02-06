d8211d64762ef9a053474c83f665443d9ce794aa
Cleber Moraes <alvesmoraesb@gmail.com>
2023-02-05 22:28:09 -0300

    Inserindo Variáveis
    Pegando Input, button e divs do html com js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var billInput = document.querySelector("#bill-input").value;
+var custonInput = document.querySelector("#custon-input").value;
+var peopleInput = document.querySelector("#people-input").value;
+
+var buttonCinco = document.querySelector("#button-cinco");
+var buttonDez = document.querySelector("#button-dez");
+var buttonQuinze = document.querySelector("#button-quinze");
+var buttonVinteCinco = document.querySelector("#button-vinte-cinco");
+var buttonCinqyuenta = document.querySelector("#button-cinqyuenta");
+
+var tipAmountResult = document.querySelector("#tip-amount-result");
+var totalResult = document.querySelector("#total-result");
