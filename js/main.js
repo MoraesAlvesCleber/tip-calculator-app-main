1838d00a9dac8d4c1e3652b0e0e12a7e120d77bc
Cleber Moraes <alvesmoraesb@gmail.com>
2023-02-09 23:15:18 -0300

    Otimizando function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ let billInput = document.querySelector("#bill-input");
 let custonInput = document.querySelector("#custon-input");
 let peopleInput = document.querySelector("#people-input");
 
-
-let buttonCinco = document.querySelector("#button-cinco");
-let buttonDez = document.querySelector("#button-dez");
-let buttonQuinze = document.querySelector("#button-quinze");
-let buttonVinteCinco = document.querySelector("#button-vinte-cinco");
-let buttonCinqyuenta = document.querySelector("#button-cinquenta");
-
 let tipAmountResult = document.querySelector("#tip-amount-result");
 let totalResult = document.querySelector("#total-result");
 
@@ -19,51 +12,23 @@ let totalResult = document.querySelector("#total-result");
 //Valor da conta / Número pessoas =  Conta de Cada Pessoa --->     100 /   2   =    50
 //Conta de Cada Pessoa = Gorjeta de Cada Pessoa --->                50 +   2.5 =    52.5
 
-//function start()// {
-// printTip();
-// printSplitAccoun();
-
-
-//}
-
-//function somapercent(id, texto){
-//   document.getElementById(id).addEventListener("click", function(){
-//   document.getElementById("#tip-amount-result").textContent = texto});    
-//}
-
-
-
-
-
+ 
 function teste(x, y) {
     
-    document.querySelector(x).addEventListener("click", function () {
-        totalResult.textContent = y * billInput / 100;
+    document.querySelector("#" + x).addEventListener("click", function () {
+        let percentage =  y * billInput.value / 100;
+        totalResult.textContent = percentage / peopleInput.value;
+        tipAmountResult.textContent = billInput.value / peopleInput.value;
     });
 }
+
 let buttonPercent = document.querySelectorAll('.button-percent');
  
+
 for (let i = 0; i < buttonPercent.length; i++) {
     let x = buttonPercent[i].id;
-    let y = buttonPercent[i].value;
-    console.log(x, y);
+    let y = buttonPercent[i].value; 
+    let z = buttonPercent[i];
+
     teste(x, y);
 }
-
-
-//function printTip() {
-
-    //let totalValue = custonInput.value * billInput.value / 100;
-
-    //let totalNumber = totalValue / peopleInput.value;
-    //tipAmountResult.textContent = totalNumber;
-//}
-
-//function printSplitAccoun() {
-
-    //let total = billInput.value / peopleInput.value;
-    //totalResult.textContent = total;
-
-//}
-
-//buttonCinco.addEventListener("click", start);
